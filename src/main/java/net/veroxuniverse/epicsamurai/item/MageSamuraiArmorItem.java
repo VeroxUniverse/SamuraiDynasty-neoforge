f0c3159a3a283ae4ede2426a84e43c54b286bf2e
stauplays <stauplays@gmail.com>
2023-05-29 15:22:20 +0200

    Ars Nouveau Compat

--- src/main/java/net/veroxuniverse/epicsamurai/item/MageSamuraiArmorItem.java
+++ src/main/java/net/veroxuniverse/epicsamurai/item/MageSamuraiArmorItem.java
@@ -6,6 +6,7 @@ import com.hollingsworth.arsnouveau.api.ArsNouveauAPI;
 import com.hollingsworth.arsnouveau.api.mana.IManaEquipment;
 import com.hollingsworth.arsnouveau.api.perk.*;
 import com.hollingsworth.arsnouveau.api.util.PerkUtil;
+import com.hollingsworth.arsnouveau.common.armor.AnimatedMagicArmor;
 import com.hollingsworth.arsnouveau.common.perk.RepairingPerk;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -33,7 +34,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-public class MageSamuraiArmorItem extends GeoArmorItem implements IManaEquipment, IAnimatable {
+public class MageSamuraiArmorItem extends AnimatedMagicArmor implements IManaEquipment, IAnimatable {
 
     private final AnimationFactory FACTORY = GeckoLibUtil.createFactory(this);
 
@@ -63,56 +64,5 @@ public class MageSamuraiArmorItem extends GeoArmorItem implements IManaEquipment
         return true;
     }
 
-    @Override
-    public void onArmorTick(ItemStack stack, Level world, Player player) {
-        if (world.isClientSide())
-            return;
-        RepairingPerk.attemptRepair(stack, player);
-        IPerkHolder<ItemStack> perkHolder = PerkUtil.getPerkHolder(stack);
-        if (perkHolder == null)
-            return;
-        for (PerkInstance instance : perkHolder.getPerkInstances()) {
-            if (instance.getPerk() instanceof ITickablePerk tickablePerk) {
-                tickablePerk.tick(stack, world, player, instance);
-            }
-        }
-    }
-
-    protected UUID getModifierForSlot(EquipmentSlot pEquipmentSlot) {
-        return ARMOR_MODIFIER_UUID_PER_SLOT[pEquipmentSlot.getIndex()];
-    }
-
-    @Override
-    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot pEquipmentSlot, ItemStack stack) {
-        ImmutableMultimap.Builder<Attribute, AttributeModifier> attributes = new ImmutableMultimap.Builder<>();
-        attributes.putAll(super.getDefaultAttributeModifiers(pEquipmentSlot));
-        if (this.slot == pEquipmentSlot) {
-            UUID uuid = getModifierForSlot(this.slot);
-            IPerkHolder<ItemStack> perkHolder = PerkUtil.getPerkHolder(stack);
-            if (perkHolder != null) {
-                attributes.put(PerkAttributes.FLAT_MANA_BONUS.get(), new AttributeModifier(uuid, "max_mana_armor", 30 * (perkHolder.getTier() + 1), AttributeModifier.Operation.ADDITION));
-                attributes.put(PerkAttributes.MANA_REGEN_BONUS.get(), new AttributeModifier(uuid, "mana_regen_armor", perkHolder.getTier() + 1, AttributeModifier.Operation.ADDITION));
-                for (PerkInstance perkInstance : perkHolder.getPerkInstances()) {
-                    IPerk perk = perkInstance.getPerk();
-                    attributes.putAll(perk.getModifiers(this.slot, stack, perkInstance.getSlot().value));
-                }
-
-            }
-        }
-        return attributes.build();
-    }
-
-    @Override
-    @OnlyIn(Dist.CLIENT)
-    public void appendHoverText(ItemStack stack, Level world, List<Component> tooltip, TooltipFlag flag) {
-        super.appendHoverText(stack, world, tooltip, flag);
-        IPerkProvider<ItemStack> perkProvider = ArsNouveauAPI.getInstance().getPerkProvider(stack.getItem());
-        if (perkProvider != null) {
-            if (perkProvider.getPerkHolder(stack) instanceof ArmorPerkHolder armorPerkHolder) {
-                tooltip.add(Component.translatable("ars_nouveau.tier", armorPerkHolder.getTier() + 1).withStyle(ChatFormatting.GOLD));
-            }
-            perkProvider.getPerkHolder(stack).appendPerkTooltip(tooltip, stack);
-        }
-    }
 
 }
