ba90a027dd97021e57d06ff58cb21fc1eabea435
stauplays <stauplays@gmail.com>
2023-06-09 00:41:31 +0200

    README update

--- src/main/java/net/veroxuniverse/epicsamurai/item/KunaiItem.java
+++ src/main/java/net/veroxuniverse/epicsamurai/item/KunaiItem.java
@@ -31,6 +31,7 @@ public class KunaiItem extends SwordItem implements Vanishable {
     public int getUseDuration(ItemStack pStack) {
         return 300;
     }
+
     public @NotNull InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
         pPlayer.getCooldowns().addCooldown(this, 20);
