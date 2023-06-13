2afbc003e2339d4e19ee7b51a089333e7b2a39bb
stauplays <stauplays@gmail.com>
2023-06-13 22:40:45 +0200

    Porting to 1.20 Removed: Demon Slayer Enchantment Onibi Entity Kitsune Projectile attack

---
+++ src/main/java/net/veroxuniverse/epicsamurai/item/armor/lib/SamuraiArmorItem.java
@@ -0,0 +1,41 @@
+package net.veroxuniverse.epicsamurai.item.armor.lib;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.object.PlayState;
+
+public class SamuraiArmorItem extends ArmorItem implements GeoItem {
+
+    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+
+    public SamuraiArmorItem(ArmorMaterial material, Type type, Properties properties) {
+        super(material, type, properties);
+    }
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+        controllerRegistrar.add(new AnimationController(this, "controller", 0, this::predicate));
+    }
+
+    private PlayState predicate(AnimationState animationState) {
+        return PlayState.STOP;
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+
+    @Override
+    public boolean canWalkOnPowderedSnow(ItemStack stack, LivingEntity wearer) {
+        return true;
+    }
+
+}
---
+++ src/main/java/net/veroxuniverse/epicsamurai/registry/CreativeTabRegistry.java
@@ -0,0 +1,62 @@
+package net.veroxuniverse.epicsamurai.registry;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.veroxuniverse.epicsamurai.EpicSamuraiMod;
+import net.veroxuniverse.epicsamurai.compat.CreateCompat;
+import net.veroxuniverse.epicsamurai.compat.DeeperDarkerCompat;
+
+public class CreativeTabRegistry {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, EpicSamuraiMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_TABS.register(EpicSamuraiMod.MOD_ID, () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup." + EpicSamuraiMod.MOD_ID))
+            .icon(() -> new ItemStack(ItemsRegistry.RED_SAMURAI_HELMET.get()))
+            .displayItems((enabledFeatures, output) -> {
+                for(RegistryObject<Item> item : ItemsRegistry.ITEMS.getEntries()){
+                    if(item.get() instanceof CustomTabs customTabBehavior){
+                        customTabBehavior.fillItemCategory(output);
+                    }else{
+                        output.accept(item.get());
+                    }
+                }
+                for(RegistryObject<Item> item : CreateCompat.CREATE_ITEMS.getEntries()){
+                    if(item.get() instanceof CustomTabs customTabBehavior){
+                        customTabBehavior.fillItemCategory(output);
+                    }else{
+                        output.accept(item.get());
+                    }
+                }
+                for(RegistryObject<Item> item : DeeperDarkerCompat.DEEPERDARKER_ITEMS.getEntries()){
+                    if(item.get() instanceof CustomTabs customTabBehavior){
+                        customTabBehavior.fillItemCategory(output);
+                    }else{
+                        output.accept(item.get());
+                    }
+                }
+
+                /* Ars Compat Removed
+
+                for(RegistryObject<Item> item : ArsNouveauCompat.ARSN_ITEMS.getEntries()){
+                    if(item.get() instanceof CustomTabs customTabBehavior){
+                        customTabBehavior.fillItemCategory(output);
+                    }else{
+                        output.accept(item.get());
+                    }
+                }
+
+                 */
+
+            })
+            .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_TABS.register(eventBus);
+    }
+}
