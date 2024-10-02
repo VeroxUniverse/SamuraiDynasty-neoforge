6475f9a7a1b422c8a3df46f43501d50362202c51
VeroxUniverse <stauplays@gmail.com>
2024-10-02 16:49:19 +0200

    armor change

--- src/main/java/net/veroxuniverse/samurai_dynasty/client/armors/samurai_armor/steel/SteelSamuraiArmorRenderer.java
+++ src/main/java/net/veroxuniverse/samurai_dynasty/client/armors/samurai_armor/steel/SteelSamuraiArmorRenderer.java
@@ -1,11 +1,11 @@
 package net.veroxuniverse.samurai_dynasty.client.armors.samurai_armor.steel;
 
 import mod.azure.azurelib.common.api.client.renderer.DyeableGeoArmorRenderer;
-import mod.azure.azurelib.common.api.client.renderer.GeoArmorRenderer;
 import mod.azure.azurelib.common.internal.common.cache.object.GeoBone;
 import mod.azure.azurelib.core.object.Color;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.DyedItemColor;
 import net.veroxuniverse.samurai_dynasty.item.armor.SteelSamuraiArmorItem;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,8 +13,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SteelSamuraiArmorRenderer extends DyeableGeoArmorRenderer<SteelSamuraiArmorItem> {
+
+    private ItemStack itemStack;
+
     public SteelSamuraiArmorRenderer() {
         super(new SteelSamuraiArmorModel());
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
     }
 
     @Override
@@ -39,40 +46,11 @@ public class SteelSamuraiArmorRenderer extends DyeableGeoArmorRenderer<SteelSamu
 
     @Override
     protected @NotNull Color getColorForBone(GeoBone bone) {
-        return Color.RED;
-    }
-
-    /*
-
-    private final Holder<ArmorMaterial> armorMaterialHolder = ArmorMaterialsRegistry.SAMURAI_STEEL;
-    private final Item.Properties properties = new Item.Properties();
-    private final DyeColor armorColor = DyeColor.RED;
-
-    private Map<SteelSamuraiArmorItem.Type, SteelSamuraiArmorItem> armorSetMap = createArmorSetMap();
-
-    private Map<SteelSamuraiArmorItem.Type, SteelSamuraiArmorItem> createArmorSetMap() {
-        Map<SteelSamuraiArmorItem.Type, SteelSamuraiArmorItem> armorSet = new HashMap<>();
-
-        armorSet.put(SteelSamuraiArmorItem.Type.HELMET, new SteelSamuraiArmorItem(armorMaterialHolder, SteelSamuraiArmorItem.Type.HELMET, properties, armorColor));
-        armorSet.put(SteelSamuraiArmorItem.Type.CHESTPLATE, new SteelSamuraiArmorItem(armorMaterialHolder, SteelSamuraiArmorItem.Type.CHESTPLATE, properties, armorColor));
-        armorSet.put(SteelSamuraiArmorItem.Type.LEGGINGS, new SteelSamuraiArmorItem(armorMaterialHolder, SteelSamuraiArmorItem.Type.LEGGINGS, properties, armorColor));
-        armorSet.put(SteelSamuraiArmorItem.Type.BOOTS, new SteelSamuraiArmorItem(armorMaterialHolder, SteelSamuraiArmorItem.Type.BOOTS, properties, armorColor));
-
-        return armorSet;
-    }
-
-    @Override
-    protected @NotNull Color getColorForBone(GeoBone bone) {
-
-        for (SteelSamuraiArmorItem armorItem : armorSetMap.values()) {
-            DyeColor color = armorItem.getColor();
-            if (color != null) {
-                return Color.ofOpaque(color.getTextColor());
-            }
+        if (itemStack != null) {
+            int color = DyedItemColor.getOrDefault(itemStack, 0xFFFF0000);
+            return new Color(color);
         }
-        return Color.ofOpaque(0xFF0000);
+        return new Color(0xFFFF0000);
     }
-
-     */
 
 }
